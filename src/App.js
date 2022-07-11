b150e1bb7e9073eb5d37c1318625ed113fe8fd12
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2022-07-12 00:29:56 +0200

    add all perfect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { objectToArray } from "helprjs";
+import _ from "lodash";
 
 function App() {
   const result = objectToArray({
@@ -10,6 +11,15 @@ function App() {
   let obj = {};
   obj[result[0]] = result[1];
   console.log(obj);
+  const objTEst = {
+    firstName: "Mike",
+    lastName: "Jones"
+  };
+  const lodashResult = _.values(objTEst);
+  console.log(lodashResult);
+  let obj1 = {};
+  obj1[lodashResult[0]] = lodashResult[1];
+  console.log("Lodash", obj1);
 
   return (
     <div className="App">
